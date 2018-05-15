63967f50e12e22bb1ab1c5314dbf78debed888c7
S. M. Hickman <sean@grump.ly>
2018-05-14 19:12:01 -0500

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,9 @@
+{ mkDerivation, base, pure-core, pure-txt, stdenv }:
+mkDerivation {
+  pname = "pure-html";
+  version = "0.7.0.0";
+  src = ./.;
+  libraryHaskellDepends = [ base pure-core pure-txt ];
+  homepage = "github.com/grumply/pure-html";
+  license = stdenv.lib.licenses.bsd3;
+}
